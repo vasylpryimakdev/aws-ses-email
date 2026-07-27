e2e55ff0ee08b8c551d7931a3c6139fa94accf3e
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-27 16:02:57 +0300

    refactor: modernize SES email handler

--- handler.js
+++ handler.js
@@ -1,8 +1,74 @@
-exports.createContact = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v4.0! Your function executed successfully!'
-    })
+"use strict";
+
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
+
+const sesClient = new SESClient({});
+
+module.exports.createContact = async (event) => {
+  console.log("Received:", event);
+
+  const { to, from, subject, message } = JSON.parse(event.body);
+
+  if (!to || !from || !subject || !message) {
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "to, from, subject or message are missing",
+      }),
+    };
+  }
+
+  const params = {
+    Destination: {
+      ToAddresses: [to],
+    },
+    Message: {
+      Subject: {
+        Data: subject,
+      },
+      Body: {
+        Text: {
+          Data: message,
+        },
+      },
+    },
+    Source: from,
   };
+
+  try {
+    await sesClient.send(new SendEmailCommand(params));
+
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Email sent successfully!",
+        success: true,
+      }),
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "The email failed to send",
+        success: false,
+      }),
+    };
+  }
 };
